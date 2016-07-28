bc989a31e27720a64009ad09ea98bb0f99db9d4b
imanhd <imanheidari56@gmail.com>
2016-07-28 17:11:34 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {iman_hy1(our_id)},--Sudo users
+    sudo_users = {iman_hy1},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
